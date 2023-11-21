5d418c4bbb0f40ab99e2bdce1d36858c3cb9d9bb
Mithu <dilder.hossain.feni@gmail.com>
2023-11-21 12:17:44 +0600

    updated a success message fade out

---
+++ Oracle Apex Docs/fade out success message.js
@@ -0,0 +1,9 @@
+// create dynamic action on page load 
+
+// execute javascript code
+
+if(apex.message.TYPE.SUCCESS='success'){
+    setTimeout(function(){ $('.t-Body-alert .t-Alert').fadeOut('slow'); }, 3000);
+};
+
+// 3000 is fade out period
