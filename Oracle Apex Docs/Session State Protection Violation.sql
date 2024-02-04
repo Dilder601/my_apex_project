8d5d78d24bdc2d65ee77189e9d45fca8e8878ac2
Mithu <dilder.hossain.feni@gmail.com>
2024-02-04 17:30:59 +0600

    Updated SMS Feature

--- Oracle Apex Docs/Session State Protection Violation.sql
+++ Oracle Apex Docs/Session State Protection Violation.sql
@@ -4,6 +4,15 @@ Execute when Page Loads:
 
     document.getElementById("P11_ID").readOnly = true;
 
+or 
+
+custom attribute:
+set this 
+
+readonly="readonly"
+
+text item will be text .
+
 
     VALUE PROTECTION : OFF
     TYPE: HIDDEN IF NECESSARY
